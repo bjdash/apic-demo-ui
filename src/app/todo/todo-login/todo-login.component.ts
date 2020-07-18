408ff14fbe0a030244db05f7e57be56101228320
Bijaya Dash <bijaya.k.dash@gmail.com>
2020-07-18 02:01:05 +0100

    build changes

--- src/app/todo/todo-login/todo-login.component.ts
+++ src/app/todo/todo-login/todo-login.component.ts
@@ -1,22 +1,31 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  FormBuilder,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-login',
   templateUrl: './todo-login.component.html',
-  styleUrls: []
+  styleUrls: [],
 })
 export class TodoLoginComponent implements OnInit {
   form: FormGroup;
   error: string;
-  loading: boolean = false
+  loading: boolean = false;
 
-  constructor(private fb: FormBuilder, private router: Router, private todoService: TodoService) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private todoService: TodoService
+  ) {
     this.form = this.fb.group({
-      username: ['kasikakudi', [Validators.required, Validators.maxLength(30)]],
-      password: ['kasikakudi', [Validators.required, Validators.maxLength(30)]]
+      username: ['', [Validators.required, Validators.maxLength(30)]],
+      password: ['', [Validators.required, Validators.maxLength(30)]],
     });
     localStorage.removeItem('token');
     localStorage.removeItem('user');
@@ -25,14 +34,14 @@ export class TodoLoginComponent implements OnInit {
   async submit() {
     console.log(this.form);
     if (this.form.valid) {
-      this.error = null
+      this.error = null;
       try {
-        const { username, password } = this.form.value
+        const { username, password } = this.form.value;
         const respData: any = await this.todoService.login(username, password);
         if (respData.token) {
           localStorage.setItem('token', respData.token);
           localStorage.setItem('user', respData.username);
-          this.todoService.updateAuthToken(respData.token)
+          this.todoService.updateAuthToken(respData.token);
           this.router.navigate(['todo', 'home']);
         } else {
           this.error = 'Login failed';
@@ -46,6 +55,5 @@ export class TodoLoginComponent implements OnInit {
     }
   }
 
-  ngOnInit(): void { }
-
+  ngOnInit(): void {}
 }
