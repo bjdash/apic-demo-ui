e7549ed374f20407a1ebd5da8a260e24a07c59e6
Bijaya Dash <bijaya.k.dash@gmail.com>
2020-07-20 14:13:15 +0100

    Added spinner

--- src/app/todo/todo-home/todo-home.component.ts
+++ src/app/todo/todo-home/todo-home.component.ts
@@ -14,7 +14,12 @@ export class TodoHomeComponent implements OnInit {
   user: string;
   sessionTime: number = 0;
   form: FormGroup;
-  todos: any[] = []
+  todos: any[] = [];
+  flags = {
+    adding: false,
+    deleting: false,
+    getting: false
+  }
 
   constructor(private todoService: TodoService, private fb: FormBuilder, private authGuardService: AuthGuardService, private router: Router) {
     this.form = this.fb.group({
@@ -26,33 +31,45 @@ export class TodoHomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.getTodos();
-    setInterval(() => {
+    const intrval = setInterval(() => {
       this.sessionTime = this.sessionTime - 1;
+      if (this.sessionTime <= 0) {
+        clearInterval(intrval);
+        this.router.navigate(['todo']);
+      }
     }, 1000)
   }
 
   getTodos() {
+    this.flags.getting = true;
     this.todoService.getTodos().subscribe((todos: any[]) => {
       this.todos = todos;
+      this.flags.getting = false;
     });
   }
 
   createTodo() {
     if (this.form.valid) {
+      this.flags.adding = true;
       this.todoService.createTodo(this.form.value.name).subscribe((todo: any[]) => {
+        this.flags.adding = false;
         this.getTodos()
       });
     }
   }
 
   deleteTodo(todoId: string) {
+    this.flags.deleting = true;
     this.todoService.deleteTodo(todoId).subscribe((todo: any[]) => {
+      this.flags.deleting = false;
       this.getTodos()
     });
   }
 
   logout() {
     this.todoService.logout().subscribe((todo: any[]) => {
+      this.router.navigate(['todo']);
+    }, error => {
       this.router.navigate(['todo']);
     });
   }
--- src/app/todo/todo-login/todo-login.component.ts
+++ src/app/todo/todo-login/todo-login.component.ts
@@ -1,28 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-import {
-  FormGroup,
-  FormControl,
-  Validators,
-  FormBuilder,
-} from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { Router } from '@angular/router';
 import { TodoService } from '../todo.service';
 
 @Component({
   selector: 'app-todo-login',
   templateUrl: './todo-login.component.html',
-  styleUrls: [],
+  styleUrls: []
 })
 export class TodoLoginComponent implements OnInit {
   form: FormGroup;
   error: string;
-  loading: boolean = false;
+  loading: boolean = false
 
-  constructor(
-    private fb: FormBuilder,
-    private router: Router,
-    private todoService: TodoService
-  ) {
+  constructor(private fb: FormBuilder, private router: Router, private todoService: TodoService) {
     this.form = this.fb.group({
       username: ['', [Validators.required, Validators.maxLength(30)]],
       password: ['', [Validators.required, Validators.maxLength(30)]],
@@ -32,28 +23,31 @@ export class TodoLoginComponent implements OnInit {
   }
 
   async submit() {
-    console.log(this.form);
     if (this.form.valid) {
       this.error = null;
+      this.loading = true;
       try {
-        const { username, password } = this.form.value;
+        const { username, password } = this.form.value
         const respData: any = await this.todoService.login(username, password);
         if (respData.token) {
           localStorage.setItem('token', respData.token);
           localStorage.setItem('user', respData.username);
-          this.todoService.updateAuthToken(respData.token);
+          this.todoService.updateAuthToken(respData.token)
           this.router.navigate(['todo', 'home']);
         } else {
           this.error = 'Login failed';
         }
+        this.loading = false;
       } catch (e) {
         console.log(e);
         this.error = e.error.message;
+        this.loading = false;
       }
     } else {
       this.error = 'Please enter valid details';
     }
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
+
 }
